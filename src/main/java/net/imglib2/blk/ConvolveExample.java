c9c42dbaa1c5076c301ba091caf353b576b59177
tpietzsch <tobias.pietzsch@gmail.com>
2021-04-08 22:17:12 +0200

    Refactor

--- src/main/java/net/imglib2/blk/ConvolveExample.java
+++ src/main/java/net/imglib2/blk/ConvolveExample.java
@@ -1,87 +1,47 @@
 package net.imglib2.blk;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 import net.imglib2.algorithm.convolution.kernel.Kernel1D;
 import net.imglib2.algorithm.gauss3.Gauss3;
-import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.util.Intervals;
-import net.imglib2.util.Util;
-import net.imglib2.view.Views;
 
 public class ConvolveExample
 {
-	static class ExpectedResults
+	private final Kernel1D[] kernels;
+
+	private final int[] targetSizeX;
+	private final int[] targetSizeY;
+	private final int[] targetSizeZ;
+
+	private final double[] targetX;
+	private final double[] targetY;
+	private final double[] targetZ;
+
+	public static void main( String[] args )
 	{
-		private final double[] sigmas;
-		private final int[] targetSize;
+		final double[] sigmas = { 8, 8, 8 };
+		final int[] targetSize = { 68, 68, 48 };
+		final RandomSourceData sourceData = new RandomSourceData( targetSize, sigmas );
 
-		private final double[] source;
-		private final Img< DoubleType > sourceImg;
+		final ExpectedResults expected = new ExpectedResults( targetSize, sigmas, sourceData.source, sourceData.sourceSize );
+		final ConvolveExample actual = new ConvolveExample( targetSize, sigmas, sourceData.source, sourceData.sourceSize );
 
-		private final double[] target;
-		private final Img< DoubleType > targetImg;
-
-		public ExpectedResults( final int[] targetSize, final double[] sigmas )
-		{
-			final int n = targetSize.length;
-
-			this.targetSize = targetSize;
-			this.sigmas = sigmas;
-
-			final int[] sizes = Gauss3.halfkernelsizes( sigmas );
-			final int[] sourceSize = new int[ n ];
-			Arrays.setAll( sourceSize, d -> targetSize[ d ] + 2 * ( sizes[ d ] - 1 ) );
-
-			target = new double[ ( int ) Intervals.numElements( targetSize ) ];
-			targetImg = ArrayImgs.doubles( target, Util.int2long( targetSize ) );
-
-			source = new double[ ( int ) Intervals.numElements( sourceSize ) ];
-			sourceImg = ArrayImgs.doubles( source, Util.int2long( sourceSize ) );
-
-			final Random random = new Random( 1 );
-			Arrays.setAll( source, i -> random.nextDouble() );
-
-			final long[] shift = new long[ n ];
-			Arrays.setAll( shift, d -> -( sizes[ d ] - 1 ) );
-			Gauss3.gauss( sigmas, Views.translate( sourceImg, shift ), targetImg, 1 );
-		}
+		double diff = 0;
+		for ( int i = 0; i < actual.targetZ.length; i++ )
+			diff += Math.abs( actual.targetZ[ i ] - expected.target[ i ] );
+		System.out.println( "diff = " + diff );
 	}
 
-//	private final double[] source;
-//	private final Img< DoubleType > sourceImg;
-//
-//	private final double[] target;
-//	private final Img< DoubleType > targetImg;
-
-	public ConvolveExample( ExpectedResults expectedResults )
+	public ConvolveExample( final int[] targetSize, final double[] sigmas, final double[] source, final int[] expectedSourceSize )
 	{
-		final Kernel1D[] kernels = Kernel1D.symmetric( Gauss3.halfkernels( expectedResults.sigmas ) );
+		kernels = Kernel1D.symmetric( Gauss3.halfkernels( sigmas ) );
 
-//		source = new double[ pw * ph * pd ];
-//		Random random = new Random( 1 );
-//		Arrays.setAll( source, i -> random.nextDouble() );
-//		sourceImg = ArrayImgs.doubles( source, pw, ph, pd );
-//
-//		final int tw = pw - 2 * ( sizes[ 0 ] - 1 );
-//		final int th = ph - 2 * ( sizes[ 1 ] - 1 );
-//		final int td = pd - 2 * ( sizes[ 2 ] - 1 );
-//		target = new double[ tw * th * td ];
-//		targetImg = ArrayImgs.doubles( target, tw, th, td );
-
-		System.out.println( "kernels = " + Arrays.toString( kernels ) );
-
-		final int[] targetSizeZ = expectedResults.targetSize;
-		final int[] targetSizeY = new int[] {
+		targetSizeZ = targetSize;
+		targetSizeY = new int[] {
 				targetSizeZ[ 0 ],
 				targetSizeZ[ 1 ],
 				targetSizeZ[ 2 ] + kernels[ 2 ].size() - 1,
 		};
-		final int[] targetSizeX = new int[] {
+		targetSizeX = new int[] {
 				targetSizeY[ 0 ],
 				targetSizeY[ 1 ] + kernels[ 1 ].size() - 1,
 				targetSizeY[ 2 ]
@@ -92,20 +52,21 @@ public class ConvolveExample
 				targetSizeX[ 2 ]
 		};
 
-		final double[] source = expectedResults.source;
-		final double[] targetX = new double[ ( int ) Intervals.numElements( targetSizeX ) ];
-		final double[] targetY = new double[ ( int ) Intervals.numElements( targetSizeY ) ];
-		final double[] targetZ = new double[ ( int ) Intervals.numElements( targetSizeZ ) ];
+		// verify source size
+		int diff = 0;
+		for ( int i = 0; i < sourceSize.length; ++i )
+			diff += Math.abs( sourceSize[ i ] - expectedSourceSize[ i ] );
+		if ( diff != 0 )
+			throw new IllegalArgumentException();
+
+		targetX = new double[ ( int ) Intervals.numElements( targetSizeX ) ];
+		targetY = new double[ ( int ) Intervals.numElements( targetSizeY ) ];
+		targetZ = new double[ ( int ) Intervals.numElements( targetSizeZ ) ];
 
 		convolveX( source, sourceSize, targetX, targetSizeX, kernels[ 0 ] );
 		convolveY( targetX, targetSizeX, targetY, targetSizeY, kernels[ 1 ] );
 		convolveZ( targetY, targetSizeY, targetZ, targetSizeZ, kernels[ 2 ] );
 
-		double diff = 0;
-		for ( int i = 0; i < targetZ.length; i++ )
-			diff += Math.abs( targetZ[ i ] - expectedResults.target[ i ] );
-
-		System.out.println( "diff = " + diff );
 	}
 
 	// sx, sy, sz    : current position in source
@@ -186,13 +147,5 @@ public class ConvolveExample
 				for ( int z = 0; z < tzl; ++z )
 					for ( int k = 0; k < kl; ++k )
 						target[ z * tyl * txl + y * txl + x ] += kernel[ k ] * source[ ( z + k ) * syl * sxl + y * sxl + x ];
-	}
-
-	public static void main( String[] args )
-	{
-		final double[] sigmas = { 8, 8, 8 };
-		final int[] targetSize = { 68, 68, 48 };
-		final ExpectedResults expected = new ExpectedResults( targetSize, sigmas );
-		new ConvolveExample( expected );
 	}
 }
