0aab28ae1c637b5f267c52da7c32c02b3d7946ae
tpietzsch <tobias.pietzsch@gmail.com>
2021-04-18 20:57:45 +0200

    refactor

--- src/main/java/net/imglib2/blk/copy/CopyExample.java
+++ src/main/java/net/imglib2/blk/copy/CopyExample.java
@@ -2,31 +2,23 @@ package net.imglib2.blk.copy;
 
 import bdv.util.Bdv;
 import bdv.util.BdvFunctions;
-import bdv.util.BdvOptions;
 import ij.IJ;
 import ij.ImagePlus;
-import java.util.Arrays;
 import java.util.List;
 import net.imglib2.RandomAccess;
 import net.imglib2.blk.copy.RangePlayground3.Range;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.AbstractByteArray;
-import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
-import net.imglib2.img.basictypeaccess.array.ByteArray;
 import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
 import net.imglib2.img.cell.CellImg;
 import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.img.list.ListRandomAccess;
 import net.imglib2.loops.LoopBuilder;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
-import net.imglib2.util.IntervalIndexer;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
-
-import net.imglib2.blk.copy.RangePlayground3;
 
 public class CopyExample
 {
@@ -37,7 +29,7 @@ public class CopyExample
 		final ImagePlus imp = IJ.openImage( fn );
 		final Img< UnsignedByteType > img = ImageJFunctions.wrapByte( imp );
 
-		final CellImg< UnsignedByteType, ? > cellImg = new CellImgFactory<>( new UnsignedByteType(), 32, 64 ).create( img );
+		final CellImg< UnsignedByteType, ? > cellImg = new CellImgFactory<>( new UnsignedByteType(), 64, 64 ).create( img );
 		LoopBuilder.setImages( img, cellImg ).forEachPixel( ( a, b ) -> b.set( a ) );
 
 		BdvFunctions.show(
@@ -50,10 +42,15 @@ public class CopyExample
 		final CellGrid cellGrid = cellImg.getCellGrid();
 		System.out.println( "cellGrid = " + cellGrid );
 
-		final int ox = -134;
-		final int oy = -57;
-		final int bw = 1600;
-		final int bh = 1600;
+		final int ox = -300;
+		final int oy = -300;
+		final int bw = 1000;
+		final int bh = 1000;
+
+//		final int ox = 54;
+//		final int oy = 54;
+//		final int bw = 84;
+//		final int bh = 84;
 
 		FindRanges[] methods = {
 				RangePlayground3::findRanges_mirror_single,
@@ -65,7 +62,7 @@ public class CopyExample
 			final byte[] data = new byte[ bw * bh ];
 			final Img< UnsignedByteType > output = ArrayImgs.unsignedBytes( data, bw, bh );
 
-			copyFast( cellImg, new int[] { ox, oy }, new int[] { bw, bh }, data, method );
+			copy( cellImg, new int[] { ox, oy }, new int[] { bw, bh }, data, method );
 
 			bdv = BdvFunctions.show(
 					output,
@@ -114,19 +111,20 @@ public class CopyExample
 			dsteps[ 0 ] = 1;
 			for ( int d = 0; d < n; ++d )
 			{
-				cells.setPosition( ranges[ d ].gridx, d );
-				lengths[ d ] = ranges[ d ].w;
+				final Range r = ranges[ d ];
+				cells.setPosition( r.gridx, d );
+				lengths[ d ] = r.w;
 
-				final int cdim = grid.getCellDimension( d, ranges[ d ].gridx );
+				final int cdim = grid.getCellDimension( d, r.gridx );
 				if ( d < n - 1 )
 				{
 					csteps[ d + 1 ] = csteps[ d ] * cdim;
 					dsteps[ d + 1 ] = dsteps[ d ] * dimensions[ d ];
 				}
-				sOffset += csteps[ d ] * ranges[ d ].cellx;
-				dOffset += dsteps[ d ] * ranges[ d ].x;
+				sOffset += csteps[ d ] * r.cellx;
+				dOffset += dsteps[ d ] * r.x;
 
-				switch( ranges[ d ].dir )
+				switch( r.dir )
 				{
 				case BACKWARD:
 					csteps[ d ] = -csteps[ d ];
@@ -159,7 +157,7 @@ public class CopyExample
 		}
 	}
 
-	private static void copyFast( final CellImg< UnsignedByteType, ? > src, final int[] min, final int[] dimensions, final byte[] dest,
+	private static void copy( final CellImg< UnsignedByteType, ? > src, final int[] min, final int[] dimensions, final byte[] dest,
 			final FindRanges findRanges )
 	{
 		final CellGrid grid = src.getCellGrid();
@@ -175,83 +173,6 @@ public class CopyExample
 			{
 				ranges[ 0 ] = range0;
 				copier.copy( ranges, dimensions, dest );
-			}
-		}
-	}
-
-	private static void copy( final CellImg< UnsignedByteType, ? > src, final int[] min, final int[] dimensions, final byte[] dest,
-			final FindRanges findRanges )
-	{
-		final CellGrid grid = src.getCellGrid();
-		final List< Range > ranges0 = findRanges.findRanges( min[ 0 ], dimensions[ 0 ], ( int ) grid.imgDimension( 0 ), grid.cellDimension( 0 ) );
-		final List< Range > ranges1 = findRanges.findRanges( min[ 1 ], dimensions[ 1 ], ( int ) grid.imgDimension( 1 ), grid.cellDimension( 1 ) );
-//		System.out.println( "ranges0 = " + ranges0 );
-//		System.out.println( "ranges1 = " + ranges1 );
-
-		for ( Range range1 : ranges1 )
-		{
-			for ( Range range0 : ranges0 )
-			{
-				final int g0 = range0.gridx;
-				final int g1 = range1.gridx;
-				final Cell cell = src.getCells().getAt( g0, g1 );
-				final byte[] srcData = ( byte[] ) ( ( ( ArrayDataAccess< ? > ) cell.getData() ).getCurrentStorageArray() );
-
-				final int c0 = range0.cellx;
-				final int c1 = range1.cellx;
-
-				final int[] cdims = new int[ 2 ];
-				cell.dimensions( cdims );
-				final int[] csteps = new int[ 2 ];
-				IntervalIndexer.createAllocationSteps( cdims, csteps );
-
-				final int cstep0;
-				switch( range0.dir )
-				{
-				case FORWARD:
-					cstep0 = csteps[ 0 ];
-					break;
-				case BACKWARD:
-					cstep0 = -csteps[ 0 ];
-					break;
-				default:
-				case STAY:
-					cstep0 = 0;
-					break;
-				}
-				final int cstep1;
-				switch( range1.dir )
-				{
-				case FORWARD:
-					cstep1 = csteps[ 1 ];
-					break;
-				case BACKWARD:
-					cstep1 = -csteps[ 1 ];
-					break;
-				default:
-				case STAY:
-					cstep1 = 0;
-					break;
-				}
-
-				final int sw0 = range0.w;
-				final int sw1 = range1.w;
-
-				final int x0 = range0.x;
-				final int x1 = range1.x;
-				final int dstep1 = dimensions[ 0 ];
-
-				final int sOffset = csteps[ 1 ] * c1 + csteps[ 0 ] * c0;
-				final int dOffset = x1 * dimensions[ 0 ] + x0;
-
-				for ( int j = 0; j < sw1; ++j )
-				{
-					for ( int i = 0; i < sw0; ++i )
-					{
-						dest[ dOffset + j * dstep1 + i ] =
-								srcData[ sOffset + j * cstep1 + i * cstep0 ];
-					}
-				}
 			}
 		}
 	}
