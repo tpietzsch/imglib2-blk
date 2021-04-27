627a1d0da59a8950affa3c41b24767a150b993c2
tpietzsch <tobias.pietzsch@gmail.com>
2021-04-27 10:43:09 +0200

    Refactor block copying

--- src/main/java/net/imglib2/blk/copy/CellImgBlocks.java
+++ src/main/java/net/imglib2/blk/copy/CellImgBlocks.java
@@ -1,6 +1,10 @@
 package net.imglib2.blk.copy;
 
+import java.util.List;
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
+import net.imglib2.blk.copy.Ranges.Range;
+import net.imglib2.img.basictypeaccess.array.AbstractByteArray;
 import net.imglib2.img.cell.AbstractCellImg;
 import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
@@ -8,8 +12,14 @@ import net.imglib2.img.cell.CellGrid;
 public class CellImgBlocks
 {
 	private final AbstractCellImg< ?, ?, ?, ? > cellImg;
-
 	private final ExtensionMethod extensionMethod;
+
+	private final CellGrid cellGrid;
+	private final int n;
+	private final int[] srcDims;
+	private final RandomAccessible< ? extends Cell< ? > > cells;
+
+	private FindRanges findRanges;
 
 	public enum ExtensionMethod
 	{
@@ -18,32 +28,150 @@ public class CellImgBlocks
 		MIRROR_DOUBLE
 	}
 
-	public CellImgBlocks( final AbstractCellImg<?, ?, ? extends Cell< ? >, ?> cellImg, ExtensionMethod extensionMethod )
+	public CellImgBlocks( final AbstractCellImg< ?, ?, ? extends Cell< ? >, ? > cellImg, ExtensionMethod extensionMethod )
 	{
 		this.cellImg = cellImg;
 		this.extensionMethod = extensionMethod;
 
-		final CellGrid cellGrid = cellImg.getCellGrid();
-		final int n = cellGrid.numDimensions();
-		final int[] srcDims = new int[ n ];
+		cellGrid = cellImg.getCellGrid();
+		n = cellGrid.numDimensions();
+		srcDims = new int[ n ];
 		for ( int d = 0; d < n; d++ )
 		{
 			// TODO check whether it fits into Integer
 			srcDims[ d ] = ( int ) cellGrid.imgDimension( d );
 		}
-		final RandomAccessible< ? extends Cell< ? > > cells = cellImg.getCells();
+		cells = cellImg.getCells();
+
+		switch ( extensionMethod )
+		{
+		case BORDER:
+			findRanges = Ranges::findRanges_border;
+			break;
+		case MIRROR_SINGLE:
+			findRanges = Ranges::findRanges_mirror_single;
+			break;
+		case MIRROR_DOUBLE:
+		default:
+			findRanges = Ranges::findRanges_mirror_double;
+			break;
+		}
 	}
 
-	public void copy( final int[] srcPos, final int[] dest, final int[] size )
+	@FunctionalInterface
+	private interface FindRanges
+	{
+		List< Range > findRanges(
+				int bx, // start of block in source coordinates (in pixels)	}
+				int bw, // width of block to copy (in pixels)
+				int iw, // source image width (in pixels)
+				int cw  // source cell width (in pixels)
+		);
+	}
+
+	public void copy( final int[] srcPos, final byte[] dest, final int[] size )
 	{
 		// find ranges
+		final List< Range >[] rangesPerDimension = new List[ n ];
+		for ( int d = 0; d < n; ++d )
+			rangesPerDimension[ d ] = findRanges.findRanges( srcPos[ d ], size[ d ], srcDims[ d ], cellGrid.cellDimension( d ) );
+
+		for ( int d = 0; d < n; ++d )
+			System.out.println( String.format( "rangesPerDimension[%d].size() = %d", d, rangesPerDimension[ d ].size() ) );
+
+		// copy data
+		final Range[] ranges = new Range[ n ];
+		final RangeCopier copier = new RangeCopier();
+		copier.setupDestSize( size );
+		copy1( rangesPerDimension, ranges, copier, dest, n - 1 );
+		// TODO: rangesPerDimension, ranges, copier should be fields, not passed as parameters
 	}
 
-
-	public static void main( String[] args )
+	private void copy1( final List< Range >[] rangesPerDimension, final Range[] ranges, final RangeCopier copier, final byte[] dest, final int d )
 	{
-
+		for ( Range range : rangesPerDimension[ d ] )
+		{
+			ranges[ d ] = range;
+			if ( d > 0 )
+				copy1( rangesPerDimension, ranges, copier, dest, d - 1 );
+			else
+				copier.copy( ranges, dest );
+		}
 	}
 
+	class RangeCopier
+	{
+		private final RandomAccess< ? extends Cell< ? > > cells = cellImg.getCells().randomAccess();
 
+		private final int[] dsteps = new int[ n ];
+		private final int[] csteps = new int[ n ];
+		private final int[] lengths = new int[ n ];
+
+		void setupDestSize( final int[] size )
+		{
+			dsteps[ 0 ] = 1;
+			for ( int d = 0; d < n - 1; ++d )
+				dsteps[ d + 1 ] = dsteps[ d ] * size[ d ];
+		}
+
+		void copy( final Range[] ranges, final byte[] dest )
+		{
+			int sOffset = 0;
+			int dOffset = 0;
+			csteps[ 0 ] = 1;
+			for ( int d = 0; d < n; ++d )
+			{
+				final Range r = ranges[ d ];
+				cells.setPosition( r.gridx, d );
+				lengths[ d ] = r.w;
+
+				if ( d < n - 1 )
+				{
+					final int cdim = cellGrid.getCellDimension( d, r.gridx );
+					csteps[ d + 1 ] = csteps[ d ] * cdim;
+				}
+				sOffset += csteps[ d ] * r.cellx;
+				dOffset += dsteps[ d ] * r.x;
+
+				switch( r.dir )
+				{
+				case BACKWARD:
+					csteps[ d ] = -csteps[ d ];
+					break;
+				case STAY:
+					csteps[ d ] = 0;
+					break;
+				}
+			}
+
+			// TODO: generic type:
+			//    Object           ArrayDataAccess< A >
+			final byte[] src = ( ( AbstractByteArray< ? > ) cells.get().getData() ).getCurrentStorageArray();
+			if ( n > 1 )
+				copy1( src, sOffset, dest, dOffset, n - 1 );
+			else
+				copy0( src, sOffset, dest, dOffset );
+		}
+
+		private void copy1( final byte[] src, final int srcPos, final byte[] dest, final int destPos, final int d )
+		{
+			final int length = lengths[ d ];
+			final int cstep = csteps[ d ];
+			final int dstep = dsteps[ d ];
+			if ( d > 1 )
+				for ( int i = 0; i < length; ++i )
+					copy1( src, srcPos + i * cstep, dest, destPos + i * dstep, d - 1 );
+			else
+				for ( int i = 0; i < length; ++i )
+					copy0( src, srcPos + i * cstep, dest, destPos + i * dstep );
+		}
+
+		private void copy0( final byte[] src, final int srcPos, final byte[] dest, final int destPos )
+		{
+			final int length = lengths[ 0 ];
+			final int cstep = csteps[ 0 ];
+			for ( int i = 0; i < length; ++i )
+				dest[ destPos + i ] = src[ srcPos + i * cstep ];
+		}
+	}
 }
---
+++ src/main/java/net/imglib2/blk/copy/Ranges.java
@@ -0,0 +1,292 @@
+package net.imglib2.blk.copy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.imglib2.blk.copy.Ranges.Direction.BACKWARD;
+import static net.imglib2.blk.copy.Ranges.Direction.FORWARD;
+import static net.imglib2.blk.copy.Ranges.Direction.STAY;
+
+public class Ranges
+{
+	public enum Direction
+	{
+		FORWARD,
+		BACKWARD,
+		STAY,
+		CONSTANT;
+	}
+
+	/**
+	 * Copy {@code w} elements to coordinates {@code x} through {@code x + w}
+	 * (exclusive) in destination, from source cell with {@code gridx} grid
+	 * coordinate, starting at coordinate {@code cellx} within cell, and from
+	 * there moving in {@code dir} for successive source elements.
+	 *
+	 * It is guaranteed that all {@code w} elements fall within the same cell.
+	 */
+	// TODO: should be flattened out instead of creating objects
+	public static class Range
+	{
+		final int gridx;
+		final int cellx;
+		final int w;
+		final Direction dir;
+		final int x;
+
+		public Range( final int gridx, final int cellx, final int w, final Direction dir, final int x )
+		{
+			this.gridx = gridx;
+			this.cellx = cellx;
+			this.w = w;
+			this.dir = dir;
+			this.x = x;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "Range{" +
+					"gridx=" + gridx +
+					", cellx=" + cellx +
+					", w=" + w +
+					", dir=" + dir +
+					", x=" + x +
+					'}';
+		}
+	}
+
+	/**
+	 * Split the requested interval into ranges covering (possibly partial)
+	 * cells of the input image. The requested interval is given by start
+	 * coordinate {@code bx} (in the extended source image) and size of the
+	 * block to copy {@code bw}, in a particular dimension. The full size of the
+	 * (non-extended) image in this dimension is given by {@code iw}, the size
+	 * of a (non-truncated) cell in this dimension is given by {@code cw}.
+	 * <p>
+	 * Out-of-bounds values are determined by border extension (clamping to
+	 * nearest pixel in the image).
+	 */
+	public static List< Range >  findRanges_border(
+			int bx, // start of block in source coordinates (in pixels)
+			int bw, // width of block to copy (in pixels)
+			int iw, // source image width (in pixels)
+			int cw  // source cell width (in pixels)
+	)
+	{
+		List< Range > ranges = new ArrayList<>();
+
+		if ( bw <= 0 )
+			return ranges;
+
+		int x = 0;
+		if ( bx < 0 )
+		{
+			int w = Math.min( bw, -bx );
+			ranges.add( new Range( 0, 0, w, STAY, x ) );
+			bw -= w;
+			bx += w; // = 0
+			x += w;
+		}
+
+		if ( bw <= 0 )
+			return ranges;
+
+		int gx = bx / cw;
+		int cx = bx - ( gx * cw );
+		while ( bw > 0 && bx < iw )
+		{
+			final int w = Math.min( bw, cellWidth( gx, cw, iw ) - cx );
+			ranges.add( new Range( gx, cx, w, FORWARD, x ) );
+			bw -= w;
+			bx += w;
+			x += w;
+			++gx;
+			cx = 0;
+		}
+
+		if ( bw <= 0 )
+			return ranges;
+
+		gx = ( iw - 1 ) / cw;
+		cx = cellWidth( gx, cw, iw ) - 1;
+		ranges.add( new Range( gx, cx, bw, STAY, x ) );
+
+		return ranges;
+	}
+
+	/**
+	 * Split the requested interval into ranges covering (possibly partial)
+	 * cells of the input image. The requested interval is given by start
+	 * coordinate {@code bx} (in the extended source image) and size of the
+	 * block to copy {@code bw}, in a particular dimension. The full size of the
+	 * (non-extended) image in this dimension is given by {@code iw}, the size
+	 * of a (non-truncated) cell in this dimension is given by {@code cw}.
+	 * <p>
+	 * Out-of-bounds values are determined by mirroring with double boundary,
+	 * i.e., border pixels are repeated.
+	 */
+	static List< Range >  findRanges_mirror_double(
+			int bx, // start of block in source coordinates (in pixels)
+			int bw, // width of block to copy (in pixels)
+			int iw, // source image width (in pixels)
+			int cw  // source cell width (in pixels)
+	)
+	{
+		List< Range > ranges = new ArrayList<>();
+
+		final int pi = 2 * iw;
+		bx = ( bx < 0 )
+				? ( bx + 1 ) % pi + pi - 1
+				: bx % pi;
+		Direction dir = FORWARD;
+		if ( bx >= iw )
+		{
+			bx = pi - 1 - bx;
+			dir = BACKWARD;
+		}
+
+		int gx = bx / cw;
+		int cx = bx - ( gx * cw );
+		int x = 0;
+		while ( bw > 0 )
+		{
+			if ( dir == FORWARD)
+			{
+				final int gxw = cellWidth( gx, cw, iw );
+				final int w = Math.min( bw, gxw - cx );
+				final Range range = new Range( gx, cx, w, FORWARD, x );
+				ranges.add( range );
+
+				bw -= w;
+				x += w;
+
+				if ( ++gx * cw >= iw ) // moving out of bounds
+				{
+					--gx;
+					cx = gxw - 1;
+					dir = BACKWARD;
+				}
+				else
+				{
+					cx = 0;
+				}
+			}
+			else // dir == BACKWARD
+			{
+				final int w = Math.min( bw, cx + 1 );
+				final Range range = new Range( gx, cx, w, BACKWARD, x );
+				ranges.add( range );
+
+				bw -= w;
+				x += w;
+
+				if ( gx == 0 ) // moving into bounds
+				{
+					cx = 0;
+					dir = FORWARD;
+				}
+				else
+				{
+					cx = cellWidth( --gx, cw, iw ) - 1;
+				}
+			}
+
+		}
+		return ranges;
+	}
+
+	/**
+	 * Split the requested interval into ranges covering (possibly partial)
+	 * cells of the input image. The requested interval is given by start
+	 * coordinate {@code bx} (in the extended source image) and size of the
+	 * block to copy {@code bw}, in a particular dimension. The full size of the
+	 * (non-extended) image in this dimension is given by {@code iw}, the size
+	 * of a (non-truncated) cell in this dimension is given by {@code cw}.
+	 * <p>
+	 * Out-of-bounds values are determined by mirroring with single boundary,
+	 * i.e., border pixels are not repeated.
+	 */
+	static List< Range >  findRanges_mirror_single(
+			int bx, // start of block in source coordinates (in pixels)
+			int bw, // width of block to copy (in pixels)
+			int iw, // source image width (in pixels)
+			int cw  // source cell width (in pixels)
+	)
+	{
+		List< Range > ranges = new ArrayList<>();
+
+		final int pi = 2 * iw - 2;
+		bx = ( bx < 0 )
+				? ( bx + 1 ) % pi + pi - 1
+				: bx % pi;
+		Direction dir = FORWARD;
+		if ( bx >= iw )
+		{
+			bx = pi - bx;
+			dir = BACKWARD;
+		}
+
+		int gx = bx / cw;
+		int cx = bx - ( gx * cw );
+		int x = 0;
+		while ( bw > 0 )
+		{
+			if ( dir == FORWARD)
+			{
+				final int gxw = cellWidth( gx, cw, iw );
+				final int w = Math.min( bw, gxw - cx );
+				final Range range = new Range( gx, cx, w, FORWARD, x );
+//				System.out.println( "range = " + range );
+				ranges.add( range );
+
+				bw -= w;
+				x += w;
+
+				if ( ++gx * cw >= iw ) // moving out of bounds
+				{
+					--gx;
+					cx = gxw - 2;
+					dir = BACKWARD;
+				}
+				else
+				{
+					cx = 0;
+				}
+			}
+			else // dir == BACKWARD
+			{
+				final int w = Math.min( bw, gx == 0 ? cx : ( cx + 1 ) );
+				final Range range = new Range( gx, cx, w, BACKWARD, x );
+//				System.out.println( "range = " + range );
+				ranges.add( range );
+
+				bw -= w;
+				x += w;
+
+				if ( gx == 0 ) // moving into bounds
+				{
+					cx = 0;
+					dir = FORWARD;
+				}
+				else
+				{
+					cx = cellWidth( --gx, cw, iw ) - 1;
+				}
+			}
+
+		}
+		return ranges;
+	}
+
+	private static int cellWidth( final int gx, final int cw, final int iw )
+	{
+		final int gw = iw / cw;
+		if ( gx < gw )
+			return cw;
+		else if ( gx == gw )
+			return iw - cw * gw;
+		else
+			throw new IllegalArgumentException();
+	}
+}
