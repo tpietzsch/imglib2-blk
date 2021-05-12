9bbe088816fdb31664290bef04c85e63f4a128e7
tpietzsch <tobias.pietzsch@gmail.com>
2021-05-12 16:10:01 +0200

    Example Gauss3 vs GaussFloatBlocked

---
+++ src/main/java/net/imglib2/blk/GaussFloatBlocked.java
@@ -0,0 +1,245 @@
+package net.imglib2.blk;
+
+import java.util.Arrays;
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorSpecies;
+import net.imglib2.algorithm.convolution.kernel.Kernel1D;
+import net.imglib2.algorithm.gauss3.Gauss3;
+import net.imglib2.util.Intervals;
+
+public class GaussFloatBlocked
+{
+	private final int n;
+	private final int[] targetSize;
+	private final int[] sourceSize;
+	private final int[] sourceOffset;
+
+	private static final int blockSize = 2048;
+
+	private final Kernel1D[] kernels;
+	private final float[][] targets;
+
+	private final int ols[];
+	private final int ils[];
+	private final int ksteps[];
+
+	public GaussFloatBlocked( final double[] sigmas )
+	{
+		kernels = Kernel1D.symmetric( Gauss3.halfkernels( sigmas ) );
+
+		n = kernels.length;
+		targets = new float[ n ][];
+		ols = new int[ n ];
+		ils = new int[ n ];
+		ksteps = new int[ n ];
+
+		targetSize = new int[ n ];
+		sourceSize = new int[ n ];
+		sourceOffset = new int[ n ];
+	}
+
+	public void setTargetSize( final int[] targetSize )
+	{
+		if ( Arrays.equals( targetSize, this.targetSize ) )
+			return;
+
+		for ( int d = 0; d < n; d++ )
+		{
+			this.targetSize[ d ] = targetSize[ d ];
+			sourceSize[ d ] = targetSize[ d ];
+		}
+
+		for ( int d = n - 1; d >= 0; --d )
+		{
+			ils[ d ] = 1;
+			for ( int dd = 0; dd < d + 1; ++dd )
+				ils[ d ] *= sourceSize[ dd ];
+
+			ols[ d ] = 1;
+			for ( int dd = d + 1; dd < n; ++dd )
+				ols[ d ] *= sourceSize[ dd ];
+
+			ksteps[ d ] = 1;
+			for ( int dd = 0; dd < d; ++dd )
+				ksteps[ d ] *= sourceSize[ dd ];
+
+			if ( d < n - 1 )
+			{
+				final int l = ( int ) Intervals.numElements( sourceSize );
+				if ( targets[ d ] == null || targets[ d ].length < l )
+					targets[ d ] = new float[ l ];
+			}
+
+			sourceSize[ d ] += kernels[ d ].size() - 1;
+			sourceOffset[ d ] = - ( kernels[ d ].size() - 1 ) / 2;
+		}
+	}
+
+	public int[] getSourceSize()
+	{
+		return sourceSize;
+	}
+
+	public int[] getSourceOffset()
+	{
+		return sourceOffset;
+	}
+
+	// optional. also other arrays can be passed to compute()
+	public float[] getSourceBuffer()
+	{
+		final int l = ( int ) Intervals.numElements( sourceSize );
+		if ( targets[ n - 1 ] == null || targets[ n - 1 ].length < l )
+			targets[ n - 1 ] = new float[ l ];
+		return targets[ n -  1 ];
+	}
+
+	public void compute( final float[] source, final float[] target )
+	{
+		for ( int d = 0; d < n; ++d )
+			convolve2(
+					d == 0 ? source : targets[ d - 1 ],
+					d == n - 1 ? target : targets[ d ],
+					kernels[ d ], ols[ d ], ils[ d ], ksteps[ d ], blockSize );
+	}
+
+	private static float[] toFloats( final double[] doubles )
+	{
+		float[] floats = new float[ doubles.length ];
+		for ( int i = 0; i < doubles.length; i++ )
+			floats[ i ] = ( float ) doubles[ i ];
+		return floats;
+	}
+
+	private static void convolve(
+			final float[] source,
+			final float[] target,
+			final Kernel1D kernel1D,
+			final int ol,
+			final int til,
+			final int kstep,
+			final int bw )
+	{
+		final float[] kernel = toFloats( kernel1D.fullKernel() );
+		final int kl = kernel.length;
+		final int sil= til + ( kl - 1 ) * kstep;
+
+//		System.out.println( "til = " + til );
+//		System.out.println( "kstep = " + kstep );
+//		System.out.println( "kernel1D = " + kernel1D.size() );
+
+//		final int bw = 1 << 7;
+		final float[] sourceCopy = new float[ bw ];
+		final float[] targetCopy = new float[ bw ];
+		final int nBlocks = til / bw;
+		final int trailing = til - nBlocks * bw;
+
+//		System.out.println( "til = " + til );
+//		System.out.println( "trailing = " + trailing );
+
+		for ( int o = 0; o < ol; ++o )
+		{
+			final int to = o * til;
+			final int so = o * sil;
+
+			for ( int b = 0; b < nBlocks; ++b )
+			{
+				final int tob = to + b * bw;
+				final int sob = so + b * bw;
+
+				Arrays.fill( targetCopy, 0 );
+				for ( int k = 0; k < kl; ++k )
+				{
+					// NB: Copy data to make auto-vectorization happen.
+					// See https://richardstartin.github.io/posts/multiplying-matrices-fast-and-slow
+					System.arraycopy( source, sob + k * kstep, sourceCopy, 0, bw );
+					line( sourceCopy, targetCopy, bw, kernel[ k ] );
+				}
+				System.arraycopy( targetCopy, 0, target, tob, bw );
+			}
+			if ( trailing > 0 )
+			{
+				final int tob = to + nBlocks * bw;
+				final int sob = so + nBlocks * bw;
+
+				Arrays.fill( targetCopy, 0 );
+				for ( int k = 0; k < kl; ++k )
+				{
+					// NB: Copy data to make auto-vectorization happen.
+					// See https://richardstartin.github.io/posts/multiplying-matrices-fast-and-slow
+					System.arraycopy( source, sob + k * kstep, sourceCopy, 0, trailing );
+					line( sourceCopy, targetCopy, trailing, kernel[ k ] );
+				}
+				System.arraycopy( targetCopy, 0, target, tob, trailing );
+			}
+		}
+	}
+
+	private static void line( final float[] source, final float[] target, final int txl, final float v )
+	{
+		for ( int x = 0; x < txl; ++x )
+			target[ x ] = Math.fma( v, source[ x ], target[ x ] );
+//			target[ x ] += v * source[ x ];
+	}
+
+	private static void convolve2(
+			final float[] source,
+			final float[] target,
+			final Kernel1D kernel1D,
+			final int ol,
+			final int til,
+			final int kstep,
+			final int bw )
+	{
+		final float[] kernel = toFloats( kernel1D.fullKernel() );
+		final int kl = kernel.length;
+		final int sil = til + ( kl - 1 ) * kstep;
+
+		final int nBlocks = til / bw;
+		final int trailing = til - nBlocks * bw;
+
+		for ( int o = 0; o < ol; ++o )
+		{
+			final int to = o * til;
+			final int so = o * sil;
+
+			for ( int b = 0; b < nBlocks; ++b )
+			{
+				final int tob = to + b * bw;
+				final int sob = so + b * bw;
+
+				Arrays.fill( target, tob, tob + bw, 0 );
+				for ( int k = 0; k < kl; ++k )
+					line2( source, sob + k * kstep, target, tob, bw, kernel[ k ] );
+			}
+			if ( trailing > 0 )
+			{
+				final int tob = to + nBlocks * bw;
+				final int sob = so + nBlocks * bw;
+
+				Arrays.fill( target, tob, tob + trailing, 0 );
+				for ( int k = 0; k < kl; ++k )
+					line2( source, sob + k * kstep, target, tob, trailing, kernel[ k ] );
+			}
+		}
+	}
+
+	private static final VectorSpecies< Float > SPECIES = FloatVector.SPECIES_PREFERRED;
+
+	private static void line2( final float[] source, final int so, final float[] target, final int to, final int l, final float v )
+	{
+		int x = 0;
+		for (; x < SPECIES.loopBound( l ); x += SPECIES.length() )
+		{
+			final FloatVector s = FloatVector.fromArray( SPECIES, source, so + x );
+			final FloatVector t = FloatVector.fromArray( SPECIES, target, to + x );
+			final FloatVector r = s.fma( FloatVector.broadcast( SPECIES, v ), t );
+			r.intoArray( target, to + x );
+		}
+
+		for ( ; x < l; ++x )
+			target[ to + x ] = Math.fma( v, source[ so + x ], target[ to + x ] );
+//			target[ x ] += v * source[ x ];
+	}
+
+}
---
+++ src/main/java/net/imglib2/blk/GaussFloatExample.java
@@ -0,0 +1,104 @@
+package net.imglib2.blk;
+
+import bdv.vistools.Bdv;
+import bdv.vistools.BdvFunctions;
+import bdv.vistools.BdvSource;
+import bdv.vistools.BdvStackSource;
+import bdv.vistools.volatiles.SharedQueue;
+import bdv.vistools.volatiles.VolatileViews;
+import ij.IJ;
+import ij.ImagePlus;
+import net.imglib2.Volatile;
+import net.imglib2.algorithm.gauss3.Gauss3;
+import net.imglib2.blk.copy.CellImgBlocks;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.cache.img.CellLoader;
+import net.imglib2.cache.img.ReadOnlyCachedCellImgFactory;
+import net.imglib2.cache.img.ReadOnlyCachedCellImgOptions;
+import net.imglib2.cache.img.SingleCellArrayImg;
+import net.imglib2.ij.img.display.imagej.ImageJFunctions;
+import net.imglib2.img.Img;
+import net.imglib2.img.cell.CellImg;
+import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+
+import static net.imglib2.blk.copy.Extension.CONSTANT;
+
+public class GaussFloatExample
+{
+	public static void main( String[] args )
+	{
+		final String fn = "/Users/pietzsch/workspace/data/e002_stack_fused-8bit.tif";
+//		final String fn = "/Users/pietzsch/workspace/data/DrosophilaWing.tif";
+//		final String fn = "/Users/pietzsch/workspace/data/leafcrop.tif";
+		final ImagePlus imp = IJ.openImage( fn );
+		final Img< UnsignedByteType > img = ImageJFunctions.wrapByte( imp );
+
+		final CellImg< FloatType, ? > cellImg = new CellImgFactory<>( new FloatType(), 64, 64, 64 ).create( img );
+		LoopBuilder.setImages( img, cellImg ).forEachPixel( ( a, b ) -> b.set( a.get() ) );
+
+		final BdvSource bdv = BdvFunctions.show(
+				cellImg,
+				"input",
+				Bdv.options() );
+		bdv.setColor( new ARGBType( 0xff0000 ) );
+		bdv.setDisplayRange( 0, 255 );
+
+//		final double[] sigmas = { 4, 4, 4 };
+		final double[] sigmas = { 8, 8, 8 };
+		final CellImgBlocks blocks = new CellImgBlocks( cellImg, CONSTANT, new FloatType( 0 ) );
+		final ThreadLocal< GaussFloatBlocked > tlgauss = ThreadLocal.withInitial( () -> new GaussFloatBlocked( sigmas ) );
+		final CellLoader< FloatType > loader = new CellLoader< FloatType >()
+		{
+			@Override
+			public void load( final SingleCellArrayImg< FloatType, ? > cell ) throws Exception
+			{
+				final int[] srcPos = Intervals.minAsIntArray( cell );
+				final float[] dest = ( float[] ) cell.getStorageArray();
+
+				final GaussFloatBlocked gauss = tlgauss.get();
+				gauss.setTargetSize( Intervals.dimensionsAsIntArray( cell ) );
+
+				final int[] sourceOffset = gauss.getSourceOffset();
+				for ( int d = 0; d < srcPos.length; d++ )
+					srcPos[ d ] += sourceOffset[ d ];
+				final int[] size = gauss.getSourceSize();
+				final float[] src = gauss.getSourceBuffer();;
+				blocks.copy( srcPos, src, size );
+				gauss.compute( src, dest );
+			}
+		};
+
+		final CachedCellImg< FloatType, ? > smoothed = new ReadOnlyCachedCellImgFactory().create(
+				Intervals.dimensionsAsLongArray( cellImg ),
+				new FloatType(),
+				loader,
+				ReadOnlyCachedCellImgOptions.options().cellDimensions( 64, 64, 64 ) );
+
+		final CachedCellImg< FloatType, ? > gauss3 = new ReadOnlyCachedCellImgFactory().create(
+				Intervals.dimensionsAsLongArray( cellImg ),
+				new FloatType(),
+				cell -> Gauss3.gauss( sigmas, Views.extendZero( cellImg ), cell, 1 ),
+				ReadOnlyCachedCellImgOptions.options().cellDimensions( 64, 64, 64 ) );
+
+		final BdvSource smoothedSrc = BdvFunctions.show(
+				VolatileViews.wrapAsVolatile( smoothed, new SharedQueue( 1 ) ), //Runtime.getRuntime().availableProcessors() ) ),
+				"smoothed",
+				Bdv.options().addTo( bdv ) );
+
+		final BdvSource gauss3Src = BdvFunctions.show(
+				VolatileViews.wrapAsVolatile( gauss3, new SharedQueue( 1 ) ), //Runtime.getRuntime().availableProcessors() ) ),
+				"gauss3",
+				Bdv.options().addTo( bdv ) );
+
+		smoothedSrc.setColor( new ARGBType( 0x00ff00 ) );
+		smoothedSrc.setDisplayRange( 0, 255 );
+		gauss3Src.setColor( new ARGBType( 0x0000ff ) );
+		gauss3Src.setDisplayRange( 0, 255 );
+	}
+}
