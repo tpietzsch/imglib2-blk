66bd25457b239339ca1ec584ab151d42693b5383
tpietzsch <tobias.pietzsch@gmail.com>
2021-05-28 15:32:33 +0200

    TEMP imglib2-cache SNAPSHOT

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module net.imglib2.blk {
 	requires jdk.incubator.vector;
 	requires net.imglib2.algorithm;
 	requires net.imglib2;
+	requires imglib2.cache;
 	requires ij;
 	requires net.imglib2.ij;
-	requires net.imglib2.cache;
 	requires bdv.vistools;
 	requires com.formdev.flatlaf;
 	requires java.desktop;
