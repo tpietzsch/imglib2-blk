1021003d165ad34133b003a158fc39da4c963ebb
tpietzsch <tobias.pietzsch@gmail.com>
2021-06-08 21:54:25 +0200

    WIP ArrayImgBlocks

---
+++ src/main/java/net/imglib2/blk/copy/ArrayImgBlocks.java
@@ -0,0 +1,81 @@
+package net.imglib2.blk.copy;
+
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+
+public class ArrayImgBlocks< T extends NativeType< T > >
+{
+	private final ThreadLocal< ArrayImgRangeCopier > copier;
+
+	// TODO: This was added out of laziness. Probably remove...
+	private final ArrayImg< T, ? > source;
+
+	public ArrayImgBlocks( final ArrayImg< T, ? > arrayImg, Extension extension )
+	{
+		this( arrayImg, extension, null );
+	}
+
+	// TODO: CONSTANT extension method should have value parameter. Would be good use-case for sealed classes instead of enum.
+	public ArrayImgBlocks( ArrayImg< T, ? > arrayImg, Extension extension, final T oobValue )
+	{
+		// TODO: store type, verify dest array type in copy(...)
+		final T type = arrayImg.createLinkedType();
+		final MemCopy memCopy;
+		final Object oob;
+		if ( type instanceof UnsignedByteType )
+		{
+			memCopy = MemCopy.BYTE;
+			final byte v = oobValue == null ? 0 : ( ( UnsignedByteType ) oobValue ).getByte();
+			oob = new byte[] { v };
+		}
+		else if ( type instanceof UnsignedShortType )
+		{
+			memCopy = MemCopy.SHORT;
+			final short v = oobValue == null ? 0 : ( ( UnsignedShortType ) oobValue ).getShort();
+			oob = new short[] { v };
+		}
+		else if ( type instanceof FloatType )
+		{
+			memCopy = MemCopy.FLOAT;
+			final float v = oobValue == null ? 0 : ( ( FloatType ) oobValue ).get();
+			oob = new float[] { v };
+		}
+		else if ( type instanceof DoubleType )
+		{
+			memCopy = MemCopy.DOUBLE;
+			final double v = oobValue == null ? 0 : ( ( DoubleType ) oobValue ).get();
+			oob = new double[] { v };
+		}
+		else
+			throw new IllegalArgumentException( type.getClass() + " is not supported" );
+
+		final Ranges findRanges = Ranges.forExtension( extension );
+		copier = ThreadLocal.withInitial( () -> new ArrayImgRangeCopier( arrayImg, findRanges, memCopy, oob ) );
+
+		source = arrayImg;
+	}
+
+	/**
+	 * @param srcPos
+	 * 		min coordinates of block to copy from src Img.
+	 * @param dest
+	 * 		destination array. Type is {@code byte[]}, {@code float[]},
+	 * 		etc, corresponding to the src Img's native type.
+	 * @param size
+	 * 		dimensions of block to copy from src Img.
+	 */
+	public void copy( final int[] srcPos, final Object dest, final int[] size )
+	{
+		copier.get().copy( srcPos, dest, size );
+	}
+
+	// TODO: This was added out of laziness. It probably should not be in the final API.
+	public ArrayImg< T, ? > source()
+	{
+		return source;
+	}
+}
---
+++ src/main/java/net/imglib2/blk/copy/ArrayImgBlocksExample.java
@@ -0,0 +1,59 @@
+package net.imglib2.blk.copy;
+
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import ij.IJ;
+import ij.ImagePlus;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.view.Views;
+
+public class ArrayImgBlocksExample
+{
+	public static void main( String[] args )
+	{
+//		final String fn = "/Users/pietzsch/workspace/data/DrosophilaWing.tif";
+		final String fn = "/Users/pietzsch/workspace/data/leafcrop.tif";
+		final ImagePlus imp = IJ.openImage( fn );
+		final Img< UnsignedByteType > img = ImageJFunctions.wrapByte( imp );
+
+		final ArrayImg< UnsignedByteType, ? > arrayImg = new ArrayImgFactory<>( new UnsignedByteType() ).create( img );
+		LoopBuilder.setImages( img, arrayImg ).forEachPixel( ( a, b ) -> b.set( a ) );
+
+		BdvFunctions.show(
+				Views.extendMirrorSingle( arrayImg ),
+				arrayImg,
+				"input",
+				Bdv.options().is2D() );
+
+//		final int ox = -300;
+//		final int oy = -300;
+//		final int bw = 1000;
+//		final int bh = 1000;
+
+		final int ox = 54;
+		final int oy = 54;
+		final int bw = 84;
+		final int bh = 84;
+
+		Bdv bdv = null;
+		for ( Extension method : Extension.values() )
+		{
+			final byte[] data = new byte[ bw * bh ];
+			final Img< UnsignedByteType > output = ArrayImgs.unsignedBytes( data, bw, bh );
+
+			final ArrayImgBlocks< ? > blocks = new ArrayImgBlocks<>( arrayImg, method, new UnsignedByteType( 128 ) );
+			blocks.copy( new int[] { ox, oy }, data, new int[] { bw, bh } );
+
+			bdv = BdvFunctions.show(
+					output,
+					"output",
+					Bdv.options().is2D().addTo( bdv ) );
+		}
+	}
+}
