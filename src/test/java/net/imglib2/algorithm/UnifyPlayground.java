9bfb460bec02c97b0a5743463331407182f70c73
tpietzsch <tobias.pietzsch@gmail.com>
2023-06-07 23:03:27 +0200

    WIP Affine2DProcessor

--- src/test/java/net/imglib2/algorithm/UnifyPlayground.java
+++ src/test/java/net/imglib2/algorithm/UnifyPlayground.java
@@ -8,6 +8,7 @@ import net.imglib2.algorithm.blocks.BlockProcessor;
 import net.imglib2.algorithm.blocks.util.BlockProcessorSourceInterval;
 import net.imglib2.blocks.TempArray;
 import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.PrimitiveType;
 import net.imglib2.util.Cast;
 import net.imglib2.util.CloseableThreadLocal;
@@ -211,34 +212,6 @@ public class UnifyPlayground
 			}
 		}
 
-		private static class NearestNeighborFloat implements TransformLine3D< float[] >
-		{
-			private NearestNeighborFloat() {}
-
-			static final NearestNeighborFloat INSTANCE = new NearestNeighborFloat();
-
-			@Override
-			public void apply( final float[] src, final float[] dest, int offset, final int length,
-					final float d0,final float d1, final float d2,
-					final int ss0, final int ss1,
-					float sf0, float sf1, float sf2)
-			{
-				sf0 += .5f;
-				sf1 += .5f;
-				sf2 += .5f;
-				for ( int x = 0; x < length; ++x )
-				{
-					final int s0 = ( int ) sf0;
-					final int s1 = ( int ) sf1;
-					final int s2 = ( int ) sf2;
-					dest[ offset++ ] = src[ s2 * ss1 + s1 * ss0 + s0 ];
-					sf0 += d0;
-					sf1 += d1;
-					sf2 += d2;
-				}
-			}
-		}
-
 		/**
 		 * Compute a destination X line for 3D.
 		 *
@@ -284,6 +257,34 @@ public class UnifyPlayground
 							: NearestNeighborDouble.INSTANCE );
 				default:
 					throw new IllegalArgumentException();
+				}
+			}
+		}
+
+		private static class NearestNeighborFloat implements TransformLine3D< float[] >
+		{
+			private NearestNeighborFloat() {}
+
+			static final NearestNeighborFloat INSTANCE = new NearestNeighborFloat();
+
+			@Override
+			public void apply( final float[] src, final float[] dest, int offset, final int length,
+					final float d0,final float d1, final float d2,
+					final int ss0, final int ss1,
+					float sf0, float sf1, float sf2)
+			{
+				sf0 += .5f;
+				sf1 += .5f;
+				sf2 += .5f;
+				for ( int x = 0; x < length; ++x )
+				{
+					final int s0 = ( int ) sf0;
+					final int s1 = ( int ) sf1;
+					final int s2 = ( int ) sf2;
+					dest[ offset++ ] = src[ s2 * ss1 + s1 * ss0 + s0 ];
+					sf0 += d0;
+					sf1 += d1;
+					sf2 += d2;
 				}
 			}
 		}
@@ -404,4 +405,136 @@ public class UnifyPlayground
 			}
 		}
 	}
+
+
+
+
+
+
+	static class Affine2DProcessor< P > extends AbstractTransformProcessor< Affine2DProcessor< P >, P >
+	{
+		private final AffineTransform2D transformToSource;
+
+		private final TransformLine2D< P > transformLine;
+
+		private final double pdest[] = new double[ 2 ];
+
+		private final double psrc[] = new double[ 2 ];
+
+		Affine2DProcessor(
+				final AffineTransform2D transformToSource,
+				final Interpolation interpolation,
+				final PrimitiveType primitiveType )
+		{
+			this( transformToSource, interpolation, primitiveType, TransformLine2D.of( interpolation, primitiveType ) );
+		}
+
+		private Affine2DProcessor(
+				final AffineTransform2D transformToSource,
+				final Interpolation interpolation,
+				final PrimitiveType primitiveType,
+				final TransformLine2D< P > transformLine )
+		{
+			super( 2, interpolation, primitiveType );
+			this.transformToSource = transformToSource;
+			this.transformLine = transformLine;
+		}
+
+		private Affine2DProcessor( Affine2DProcessor< P > processor )
+		{
+			super( processor );
+			transformToSource = processor.transformToSource;
+			transformLine = processor.transformLine;
+		}
+
+		@Override
+		Affine2DProcessor newInstance()
+		{
+			return new Affine2DProcessor( this );
+		}
+
+		@Override
+		RealInterval estimateBounds( final Interval interval )
+		{
+			return transformToSource.estimateBounds( interval );
+		}
+
+		// specific to 3D
+		@Override
+		public void compute( final P src, final P dest )
+		{
+			final float d0 = transformToSource.d( 0 ).getFloatPosition( 0 );
+			final float d1 = transformToSource.d( 0 ).getFloatPosition( 1 );
+			final int ds0 = destSize[ 0 ];
+			final int ss0 = sourceSize[ 0 ];
+			pdest[ 0 ] = destPos[ 0 ];
+			int i = 0;
+			for ( int y = 0; y < destSize[ 1 ]; ++y )
+			{
+				pdest[ 1 ] = y + destPos[ 1 ];
+				transformToSource.apply( pdest, psrc );
+				float sf0 = ( float ) ( psrc[ 0 ] - sourcePos[ 0 ] );
+				float sf1 = ( float ) ( psrc[ 1 ] - sourcePos[ 1 ] );
+				transformLine.apply( src, dest, i, ds0, d0, d1, ss0, sf0, sf1 );
+				i += ds0;
+			}
+		}
+
+		/**
+		 * Compute a destination X line for 3D.
+		 *
+		 * @param <P>
+		 * 		input/output primitive array type (i.e., float[] or double[])
+		 */
+		@FunctionalInterface
+		interface TransformLine2D< P >
+		{
+
+			/**
+			 * @param src
+			 * 		flattened source data
+			 * @param dest
+			 * 		flattened dest data
+			 * @param offset
+			 * 		offset (into {@code dest}) of the line to compute
+			 * @param length
+			 * 		length of the line to compute (in {@code dest})
+			 * @param d0
+			 * 		partial differential vector in X of the transform (X component)
+			 * @param d1
+			 * 		partial differential vector in X of the transform (Y component)
+			 * @param ss0
+			 * 		length of a source line (size_X)
+			 * @param sf0
+			 * 		position of the first sample on the line (transformed into source)
+			 * @param sf1
+			 * 		position of the first sample on the line (transformed into source)
+			 */
+			void apply( P src, P dest, int offset, int length,
+					float d0, float d1,
+					int ss0,
+					float sf0, float sf1 );
+
+			static < P > TransformLine2D< P > of(
+					final Interpolation interpolation,
+					final PrimitiveType primitiveType )
+			{
+				switch ( primitiveType )
+				{
+				case FLOAT:
+					TODO make these implementations:
+//					return Cast.unchecked( interpolation == Interpolation.NLINEAR
+//							? NLinearFloat.INSTANCE
+//							: NearestNeighborFloat.INSTANCE );
+				case DOUBLE:
+//					return Cast.unchecked( interpolation == Interpolation.NLINEAR
+//							? NLinearDouble.INSTANCE
+//							: NearestNeighborDouble.INSTANCE );
+				default:
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+	}
+
 }
